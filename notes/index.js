2e2b46519aa9978ce0d3eca4fb56b62455b579e2
Sunil.A.Kumar <Sunil.A.Kumar@Geminisolutions.com>
2023-01-31 21:17:55 +0530

    assignment in branch

--- notes/index.js
+++
@@ -1,8 +0,0 @@
-function validateForm(){
-    var x = document.forms["myForm"]["fname"].value;
-    if(x == ""){
-        alert("Name must be filled out");
-        return false;
-    }
-}
-// if validate form is returning false than action attribute in the form will not be called else called.
